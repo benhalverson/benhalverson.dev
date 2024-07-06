e95945156749def6d59478acb4120905144b9496
Ben Halverson <benhalverson33@gmail.com>
2024-07-05 20:00:02 -0700

    reformat

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withFetch()), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideHttpClient(withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withViewTransitions()),
+    provideClientHydration(),
+  ],
 };
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, HostBinding, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  darkMode = signal(false)
+
+  @HostBinding('class.dark') get mode() {
+    return this.darkMode;
+  }
+
 
 }
