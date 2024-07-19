30e061fc2c165da94e01bab9ec7627802386c477
Ben Halverson <benhalverson33@gmail.com>
2024-07-18 23:13:27 -0700

    fixed contact issue

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,27 +1,14 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-import { CalendlyService } from '../services/calendly.service';
-
+import { Component,  OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
-  constructor(private readonly calendlyService: CalendlyService, @Inject(PLATFORM_ID) private readonly platformId: Object) { }
-
-  ngAfterViewInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      this.calendlyService.initIlineWidget({
-        url: 'https://calendly.com/benhalverson/15min',
-        parentElement: document.getElementById('calendly-inline-widget'),
-        inlineStyles: true,
-        embededType: 'Inline'
-      });
-    }
+  ngOnInit(): void {
+    window.location.href = 'https://calendly.com/benhalverson/15min';
   }
-
 }
