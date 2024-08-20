f6f86527230bdd629d98eb92e0828c4a46033799
Ben Halverson <benhalverson33@gmail.com>
2024-08-19 20:26:59 -0700

    added healthcheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, HostBinding, inject, signal } from '@angular/core';
+import { Component, HostBinding, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DarkModeService } from './services/dark-mode.service';
 import { CommonModule } from '@angular/common';
 import { KonamiService } from './services/konami.service';
+import { HealthService } from './health.service';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,13 @@ import { KonamiService } from './services/konami.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   darkModeService: DarkModeService = inject(DarkModeService);
   konamiService: KonamiService = inject(KonamiService);
+  healthService: HealthService = inject(HealthService);
+
+  ngOnInit() {
+    this.healthService.getHealth().subscribe();
+  }
 }
